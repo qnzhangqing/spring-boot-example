a48fc5c523aaff28cc001a3c009a248e5fd67353
qnzhangqing <qnzhangqing@163.com>
2018-06-26 21:04:53 +0800

    提交

--- src/main/java/com/fyqz/controller/HelloController.java
+++ src/main/java/com/fyqz/controller/HelloController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String helloSpringBoot(){
-        return "Hello SpringBoot!";
+        return "Hello SpringBootsss!";
     }
 }
