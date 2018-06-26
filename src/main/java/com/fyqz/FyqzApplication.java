f35bb5cce2a722bfc95cd1f5a9d63d8e0a1829ab
qnzhangqing <qnzhangqing@163.com>
2018-06-26 20:57:41 +0800

    import

---
+++ src/main/java/com/fyqz/FyqzApplication.java
@@ -0,0 +1,12 @@
+package com.fyqz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyqzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FyqzApplication.class, args);
+    }
+}
